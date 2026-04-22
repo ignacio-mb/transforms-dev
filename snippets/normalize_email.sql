6d208b20133377b8e0592645adbd57e9e7579981
Metabase Library <library@metabase.com>
2026-04-22 12:33:14 +0000

    Exported from Metabase

--- snippets/normalize_email.sql
+++
@@ -1,7 +0,0 @@
--- Snippet: normalize_email
--- Extracts and lowercases the domain from an email address.
--- Usage in transforms: {{ snippet: normalize_email }}
---
--- Expects a column called `email` in scope.
-
-lower(split_part(email, '@', 2))
